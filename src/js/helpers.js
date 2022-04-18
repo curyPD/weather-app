105646cf41362d4e67aca6ab7753ab719896256f
curyPD <romandruzhinin2003@gmail.com>
2022-04-18 19:57:14 +0300

    Hid the API key

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -5,3 +5,6 @@ export const convertCtoF = function (tempC) {
 export const convertFtoC = function (tempF) {
   return (tempF - 32) / 1.8;
 };
+
+// API key: 45342ac40cb9ef695441b927f8ac5ee5
+// Password: JbWB)AHJ/t%,83Q
